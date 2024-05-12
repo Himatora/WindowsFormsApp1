50d589919f28501bb524601f8a6df3f717491d5b
Liubov <conterlot33@gmail.com>
2024-05-12 22:06:11 +0800

    GravityPoint

---
+++ WindowsFormsApp1/Emitter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Emitter
+    {
+        public float GravitationX = 0;
+        public float GravitationY = 0;
+        public List<Point> gravityPoints = new List<Point>(); // тут буду хранится точки притяжения
+        List<Particle> particles = new List<Particle>();
+            public int MousePositionX;
+            public int MousePositionY;
+
+            public void UpdateState()
+            {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1;
+                if (particle.Life < 0)
+                {
+                    particle.Life = 20 + Particle.rand.Next(100);
+
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    foreach (var point in gravityPoints)
+                    {
+                        // и так считаем вектор притяжения к точке
+                        float gX = point.X - particle.X;
+                        float gY = point.Y - particle.Y;
+
+                        // считаем квадрат расстояния между частицей и точкой r^2
+                        float r2 = (float)Math.Max(100, gX * gX + gY * gY);//делаем не меньше 100, чтобы не было деления на 0 и очень большого ускорения
+                        float M = 100; // сила притяжения к точке, пусть 100 будет
+
+                        // пересчитываем вектор скорости с учетом притяжения к точке
+                        particle.SpeedX += (gX) * M / r2;
+                        particle.SpeedY += (gY) * M / r2;
+                    }
+
+                    // гравитация воздействует на вектор скорости, поэтому пересчитываем его
+                    particle.SpeedX += GravitationX;
+                    particle.SpeedY += GravitationY;
+
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                }
+            }
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500)
+                {
+                    var particle = new ParticleColorful();
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta); ;
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+            public void Render(Graphics g)
+            {
+                foreach (var particle in particles)
+                {
+                    particle.Draw(g);
+                }
+                foreach (var point in gravityPoints)
+                {
+                    g.FillEllipse(
+                        new SolidBrush(Color.Red),
+                        point.X - 5,
+                        point.Y - 5,
+                        10,
+                        10
+                    );
+                }
+        }
+    }
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,76 +12,28 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        List<Particle> particles = new List<Particle>();
+        Emitter emitter = new Emitter();
         public Form1()
         {
             InitializeComponent();
             //привязываем изображения
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+            emitter.gravityPoints.Add(new Point(
+            picDisplay.Width / 2, picDisplay.Height / 2));
+            emitter.gravityPoints.Add(new Point(
+      (int)(picDisplay.Width * 0.75), picDisplay.Height / 2));
 
-            /*for (var i = 0; i < 500; ++i)
-            {
-                var particle = new Particle();
-                particle.X = picDisplay.Image.Width / 2;
-                particle.Y = picDisplay.Image.Height / 2;
-
-                particles.Add(particle);
-            }*/
+            emitter.gravityPoints.Add(new Point(
+               (int)(picDisplay.Width * 0.25), picDisplay.Height / 2));
         }
-        private void UpdateState()
-        {
-            foreach(var particle in particles)
-            {
-                particle.Life -= 1;
-                if(particle.Life < 0)
-                {
-                    particle.Life = 20 + Particle.rand.Next(100);
-
-                    particle.X=MousePositionX;
-                    particle.Y=MousePositionY;
-                    var direction = (double)Particle.rand.Next(360);
-                    var speed = 1 + Particle.rand.Next(10);
-
-                    particle.SpeedX=(float)(Math.Cos(direction/180*Math.PI)*speed);
-                    particle.SpeedY =-(float)(Math.Sin(direction / 180 * Math.PI) * speed);
-                    particle.Radius=2+ Particle.rand.Next(10);
-                }
-                else {
-                    particle.X += particle.SpeedX;
-                    particle.Y += particle.SpeedY;
-                } 
-            }
-            for(var i = 0; i < 10; ++i)
-            {
-                if (particles.Count < 500)
-                {
-                    var particle = new ParticleColorful();
-                    particle.FromColor = Color.Yellow;
-                    particle.ToColor = Color.FromArgb(0, Color.Magenta); ;
-                    particle.X = MousePositionX;
-                    particle.Y=MousePositionY;
-                    particles.Add(particle);
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-        private void Render(Graphics g)
-        {
-            foreach(var particle in particles)
-            {
-               particle.Draw(g);
-            }
-        }
+        
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateState();
+           emitter.UpdateState();
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
                 g.Clear(Color.Black);
-                Render(g);
+                emitter.Render(g);
             }
             picDisplay.Invalidate();
         }
@@ -89,8 +41,8 @@ namespace WindowsFormsApp1
         private int MousePositionY = 0;
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            MousePositionX=e.X;
-            MousePositionY=e.Y;
+            emitter.MousePositionX =e.X;
+            emitter.MousePositionY =e.Y;
         }
     }
 }
